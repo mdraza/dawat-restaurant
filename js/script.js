6a36c9e777e3915d29e4b10c074651a6624f5882
Razaullah <raza@arenapv.com>
2020-01-16 18:03:10 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -12,3 +12,25 @@ $(document).ready(function () {
         $(window).scrollTop() >= t.height() ? t.addClass("nav-scroll") : t.removeClass("nav-scroll")
     })
 });
+
+/******* WOW Js *******/
+new WOW().init();
+
+/******* Owl Carousel *******/
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
