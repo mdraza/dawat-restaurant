bfaade85793ad5f856d69dd78c671b5e203363b9
Razaullah <raza@arenapv.com>
2020-01-17 18:07:08 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -33,4 +33,37 @@ $('.owl-carousel').owlCarousel({
             items:1
         }
     }
+});
+
+/******* Active Menu Filter Plugin  *******/
+var filterizd = $('.filtr-container').filterizr({
+
+});
+/******* Filter Button Active Class *******/
+var header = document.getElementById("workFilter");
+var btns = header.getElementsByClassName("wrk");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active_wrk");
+        current[0].className = current[0].className.replace(" active_wrk", "");
+        this.className += " active_wrk";
+    });
+}
+/********* Our Team ********/
+$('.owl-carouselTwo').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 4
+        },
+        1000: {
+            items: 5
+        }
+    }
 })
