25a77bc951ceaac8baa18b84e70bcab7f500ee9f
Razaullah <raza@arenapv.com>
2020-01-20 15:55:59 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-/***********  Navigation  **************/
+/*************************  Navigation  *********************/
 $(document).ready(function () {
     $('.menu-toggle').click(function () {
         $('nav').toggleClass('active');
     })
 });
 
-/****  Sticky Navbar  ****/
+/************************  Sticky Navbar  *******************/
 $(document).ready(function () {
     $(window).on("scroll", function () {
         var t = $(".navigation");
@@ -13,10 +13,10 @@ $(document).ready(function () {
     })
 });
 
-/******* WOW Js *******/
+/************************* WOW Js ***************************/
 new WOW().init();
 
-/******* Owl Carousel *******/
+/************************ Owl Carousel *********************/
 $('.owl-carouselOne').owlCarousel({
     loop: true,
     margin: 10,
@@ -35,11 +35,12 @@ $('.owl-carouselOne').owlCarousel({
     }
 });
 
-/******* Active Menu Filter Plugin  *******/
+/**************** Active Menu Filter Plugin  ****************/
 var filterizd = $('.filtr-container').filterizr({
 
 });
-/******* Filter Button Active Class *******/
+
+/*********************** Filter Button Active Class *********/
 var header = document.getElementById("workFilter");
 var btns = header.getElementsByClassName("wrk");
 for (var i = 0; i < btns.length; i++) {
@@ -49,7 +50,7 @@ for (var i = 0; i < btns.length; i++) {
         this.className += " active_wrk";
     });
 }
-/********* Our Team ********/
+/************************ Our Team *************************/
 $('.owl-carouselTwo').owlCarousel({
     loop: true,
     margin: 10,
@@ -69,7 +70,7 @@ $('.owl-carouselTwo').owlCarousel({
         }
     }
 })
-/********* Latest News Section ********/
+/*********************** Latest News Section ****************/
 $('.owl-carouselThree').owlCarousel({
     loop: true,
     margin: 10,
@@ -90,7 +91,7 @@ $('.owl-carouselThree').owlCarousel({
     }
 });
 
-/********* Gallery Magnific Pop ********/
+/************************* Gallery Magnific Pop ***************/
 $('.view-image').magnificPopup({
     type: 'image',
     gallery: {
@@ -102,3 +103,35 @@ $('.view-image').magnificPopup({
         easing: 'ease-in-out',
     },
 });
+
+/***************************  Preloader  **********************/
+// $("#status").fadeOut();
+$("#preloader")
+  .delay(800)
+  .fadeOut("slow");
+$("body")
+  .delay(800)
+  .css({ overflow: "visible" });
+
+
+/***************************  Smooth Scroll  **********************/
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 70
+        }, 700
+        );
+      }
+    }
+  });
